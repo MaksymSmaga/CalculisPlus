c9b2f15f02b44b9bbcbb102f4afbc07c40873d27
Егор Смирнов <egorsmirn@outlook.com>
2022-06-07 23:35:26 +0400

    Implement functions SMA/WMA/EMA and its tests; Change name of IF function

--- Calculis.Functions/Temporal/EmaFunction.cs
+++ Calculis.Functions/Temporal/EmaFunction.cs
@@ -10,14 +10,18 @@ namespace Calculis.Functions
         {
             Function = () =>
             {
-                double alpha = args[1].Value;
-                double ema_t = _cash[0].Value;
+                //double alpha = args[1].Value;
+                //double ema_t = _cash[0].Value;
+                //for (int i = 1; i < args.Count; i++)
+                //ema_t = alpha * args[0].Value + (1 - alpha) * _cash[0].Value;
 
-                for (int i = 1; i < args.Count; i++)
-                    ema_t = alpha * args[i].Value + (1 - alpha) * ema_t;
-
-                return ema_t;
+                return args[1].Value * args[0].Value + (1 - args[1].Value) * _cash[1].Value;
             };
+        }
+
+        protected override void Initialize()
+        {
+            InitializeCash(2);
         }
     }
 }
