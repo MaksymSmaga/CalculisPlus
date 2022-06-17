f6b6c736c68f49c7ec46c0635407299b9c7a14ad
Егор Смирнов <egorsmirn@outlook.com>
2022-06-17 23:09:50 +0400

    Add FunctionInfo class

--- Calculis.Functions/Normal/AbsFunction.cs
+++ Calculis.Functions/Normal/AbsFunction.cs
@@ -1,4 +1,5 @@
 ﻿using Calculis.Core;
+using Calculis.Core.Items;
 using System;
 using System.Collections.Generic;
 
@@ -7,6 +8,8 @@ namespace Calculis.Functions
     [ArgumentsNumber(1)]
     internal sealed class AbsFunction : NormalFunction
     {
+        //public override FunctionInfo Info { get; protected set; }
+
         public AbsFunction(IList<IValueItem> args) : base(args)
         {
             Function = () => Math.Abs(args[0].Value);
